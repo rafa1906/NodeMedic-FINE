619892cd4807a54d029cad4f9ecf8553677a3f67
rafa1906 <rafaelgonrg196@gmail.com>
2025-05-26 14:33:25 -0400

    added call/apply/bind fix tests

--- tests/unit_jalangi/tests/native_functions.ts
+++ tests/unit_jalangi/tests/native_functions.ts
@@ -1,7 +1,7 @@
 import { __jalangi_assert_taint_true__, __jalangi_assert_taint_false__, 
     __jalangi_set_taint__, __jalangi_set_prop_taint__, __jalangi_assert_prop_taint_false__,
     __jalangi_assert_prop_taint_true__} from "../../taint_header";
-import { test_suite, test_one } from "../../test_header";
+import { test_suite, test_one, test_assert } from "../../test_header";
 
 
 test_suite("---------- parseInt --------", function() {
@@ -57,5 +57,37 @@ test_suite("---------- isNaN --------", function() {
 
     test_one("isNaN(a) should be tainted", function () {
         __jalangi_assert_taint_true__(b);
+    });
+});
+
+test_suite("---------- call/apply/bind --------", function() {
+    let a = 2;
+
+    test_one("Setting taint on a", function() {
+        __jalangi_set_taint__(a);
+    });
+
+    let t1 = Object.prototype.toString.call(a);
+
+    // With fix, correctly returns '[object Number]'
+    // Without fix, would return '[object Object]'
+    test_one("toString.call(a) should return [object Number]", function () {
+        test_assert(t1 === '[object Number]');
+    });
+
+    let t2 = Object.prototype.toString.apply(a);
+
+    // With fix, correctly returns '[object Number]'
+    // Without fix, would return '[object Object]'
+    test_one("toString.apply(a) should return [object Number]", function () {
+        test_assert(t2 === '[object Number]');
+    });
+
+    let t3 = Object.prototype.toString.bind(a)();
+
+    // With fix, correctly returns '[object Number]'
+    // Without fix, would return '[object Object]'
+    test_one("toString.bind(a)() should return [object Number]", function () {
+        test_assert(t3 === '[object Number]');
     });
 });
